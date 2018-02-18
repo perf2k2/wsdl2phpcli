c25b8bb022bc5c58490788919422d54e6578abbb
perf2k2 <perf2k2@gmail.com>
2018-02-18 16:45:59 +0300

    Working code

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 <?php
+
+$currentDir = __DIR__;
+$url = $argv[1];
+
+require "$currentDir/vendor/autoload.php";
+
+if (preg_match("/^(http|https):\/\/([^?]+)?/", $url, $matches) !== 1) {
+    throw new RuntimeException('Unable to parse url');
+}
+
+$fileName = str_replace('/', '.', $matches[2]);
+$sourceDir = "$currentDir/source/";
+$newSourceFileName = $sourceDir . $fileName;
+
+copy($url, $newSourceFileName);
+
+$generator = new \Wsdl2PhpGenerator\Generator();
+$generator->generate(
+    new \Wsdl2PhpGenerator\Config(array(
+        'inputFile' => $newSourceFileName,
+        'outputDir' => "$currentDir/output/$fileName",
+    ))
+);
